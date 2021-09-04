ef220a36444ea8da28de6187d1995a8c1beb0872
RMAI-PLEUM <oyashi11110@gmail.com>
2021-09-04 19:47:40 +0700

    add stack Class

---
+++ lab3_week3/StackClass.py
@@ -0,0 +1,21 @@
+class Stack:
+    def __init__(self,list = None):
+        if list == None:
+            self.items = []
+        else:
+            self.items = list
+    
+    def push(self,i):
+        self.items.append(i)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[-1]
+    
+    def isEmpty(self):
+        return self.items == None
+
+    def size(self):
+        return len(self.items)
