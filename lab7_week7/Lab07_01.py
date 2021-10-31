4561a0cb7c93490e143286b928aaf42b74e590c8
RMAI-PLEUM <oyashi11110@gmail.com>
2021-10-31 20:33:51 +0700

    add lab7

---
+++ lab7_week7/Lab07_01.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        if self.root == None:
+            new_node = Node(data)
+            self.root = new_node
+            return
+        else:
+            p = self.root
+            while True:
+                if data >= p.data:
+                    if p.right == None:
+                        p.right = Node(data)
+                        break
+                    else:
+                        p =  p.right
+                if data < p.data:
+                    if p.left == None:
+                        p.left = Node(data)
+                        break
+                    else:
+                        p = p.left
+
+        return self.root
+
+    def printTree(self, node, level = 0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+T = BST()
+inp = [int(i) for i in input('Enter Input : ').split()]
+for i in inp:
+    root = T.insert(i)
+T.printTree(root)
